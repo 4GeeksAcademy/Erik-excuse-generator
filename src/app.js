7ffdde32474916314dc6f932dc1098195de704e1
Erik Martin <erikmartinfernandez@gmail.com>
2024-10-16 18:38:40 +0000

    Entrega excuse generator Erik

--- src/app.js
+++ src/app.js
@@ -6,6 +6,31 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The mecanic", "My uncle", "The dog", "My sister", "The grandpa"];
+
+  let action = ["make ", "go", "eat", "peed", "broke"];
+
+  let what = ["the car", "my homework", "biscuits", "spain", "the tv"];
+
+  let when = [
+    "today.",
+    "before the exam.",
+    "after the party.",
+    "in vacations.",
+    "yesterday."
+  ];
+
+  let randomWho = Math.floor(Math.random() * who.length);
+  let randomAction = Math.floor(Math.random() * action.length);
+  let randomWhat = Math.floor(Math.random() * what.length);
+  let randomWhen = Math.floor(Math.random() * when.length);
+
+  document.querySelector("#excuse").innerHTML =
+    who[randomWho] +
+    " " +
+    action[randomAction] +
+    " " +
+    what[randomWhat] +
+    " " +
+    when[randomWhen];
 };
